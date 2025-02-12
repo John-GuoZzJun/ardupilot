dbf7684c0cc5586a4a2b7d95cbcf0cf0069f5793
Go Czyczyun <734794435@qq.com>
2025-02-12 15:18:36 +0800

    添加参数到五角星航线测试
    成功将参数添加到飞控代码中，以实现在MissionPlanner中直接改变变量的数值，比如说在此任务中能修改五角星的半径。

--- ArduCopter/mode_DrawStar.cpp
+++ ArduCopter/mode_DrawStar.cpp
@@ -21,7 +21,8 @@ bool ModeDrawStar::init(bool ignore_checks)
 
 void ModeDrawStar::generate_path() // 生成五角星航线
 {
-    float radius_cm = 1000.0;
+    //float radius_cm = 1000.0;
+    float radius_cm = g2.star_radius_cm; // 在Parameters中新增的变量
 
     wp_nav->get_wp_stopping_point(path[0]);
 
